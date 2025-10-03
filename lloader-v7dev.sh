0239d6e9a646549e14c94a8137dccffcaaf78f51
Francesco <big.p.mod@gmail.com>
2025-10-03 10:37:52 +0200

    Refactor installation script and cleanup commands
    Updated echo statements and ensured only fuse3 is used during installation. Removed redundant cd command to prevent leaving the repository.

--- lloader-v7dev.sh
+++ lloader-v7dev.sh
@@ -3,11 +3,11 @@ set -e
 
 echo "****************************************************************************"
 echo "*  ██╗     ██╗███╗   ██╗██████╗ ██████╗ ███████╗██████╗  ██████╗ ██╗  ██╗  *"
-echo "*  ██║     ██║████╗  ██║██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔════╝ ██║  ██║  *"
+echo "*  ██║     ██║████╗  ██║██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔════╝ ██║  ██║ [...]"
 echo "*  ██║     ██║██╔██╗ ██║██║  ██║██████╔╝█████╗  ██████╔╝██║  ███╗███████║  *"
 echo "*  ██║     ██║██║╚██╗██║██║  ██║██╔══██╗██╔══╝  ██╔══██╗██║   ██║██╔══██║  *"
-echo "*  ███████╗██║██║ ╚████║██████╔╝██████╔╝███████╗██║  ██║╚██████╔╝██║  ██║  *"
-echo "*  ╚══════╝╚═╝╚═╝  ╚═══╝╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝  *"
+echo "*  ███████╗██║██║ ╚████║██████╔╝██████╔╝███████╗██║  ██║╚██████╔╝██║  [...]"
+echo "*  ╚══════╝╚═╝╚═╝  ╚═══╝╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝ [...]"
 echo "*                                                                          *"
 echo "*           Welcome to the Automated Package Installer                     *"
 echo "*                  Lindbergh Loader 2.1.x compliant                        *"
@@ -41,8 +41,11 @@ echo "Running package installation..."
 sudo dpkg --add-architecture i386
 sudo apt update
 
+# Ensure only fuse3 is used (remove legacy fuse if present)
+sudo apt-get purge -y fuse || true
+
 sudo apt -y install --no-install-recommends \
-  build-essential gcc-multilib g++-multilib cmake fuse fuse3 freeglut3-dev:i386 libvdpau1:i386 libstdc++5:i386 libxmu6:i386 \
+  build-essential gcc-multilib g++-multilib cmake fuse3 freeglut3-dev:i386 libvdpau1:i386 libstdc++5:i386 libxmu6:i386 \
   libpcsclite1:i386 libncurses5:i386 unzip libsndio-dev libsndio-dev:i386 pulseaudio-utils:i386 zlib1g:i386 libgpg-error0:i386 \
   libasound2 libasound2-dev libasound2:i386 libasound2-dev:i386 libfreetype6-dev:i386 libdbus-1-dev libpulse-dev libdbus-1-dev:i386 \
   libudev-dev:i386 libxcursor-dev:i386 libxfixes-dev:i386 libxi-dev:i386 libxrandr-dev:i386 libxss-dev:i386 libxxf86vm-dev:i386 git libvulkan1:i386 \
@@ -116,9 +119,9 @@ cd build
 cmake ../ -DCMAKE_C_FLAGS=-m32 -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=off
 make -j4
 sudo make install
-cd ../../../
+cd ../../../  # This returns to repo root (lindbergh-loader)
 
-cd ..
+# (Removed the extra: cd ..  which was causing us to leave the repo)
 
 echo "Building and installing Lindbergh Loader..."
 make
